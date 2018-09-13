71ac10b2ca81af7a584bdce9de46608e7b36fb5a
Thibault JP <thibault60000@gmail.com>
2018-09-13 09:32:05 +0200

    test

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://thibault60000.github.io/vue-favorite/',
+    assetsPublicPath: '/vue-favorite',
 
     /**
      * Source Maps
